eed175997b8f7e3d823c47f661bd7bf41fc5cf64
BeatLink <beatlink+git@simplelogin.co>
2021-10-20 22:24:31 -0500

    Schedule Viewing now complete

--- src/GUI/Panel/Panel.ts
+++ src/GUI/Panel/Panel.ts
@@ -1,52 +1,51 @@
 /* Imports *****************************************************************************************************************************************/
 import joplin from 'api';
 import {SettingItemType, ToolbarButtonLocation } from 'api/types';
+import { getTodos } from '../../Logic/joplin';
 
 var panel = null;
 
 /* setupPanel ***************************************************************************************************************************************
-Sets up the schedule panel
+Sets up the panel
 */
 export async function setupPanel(){                 
     async function createPanel(){
-        panel = await joplin.views.panels.create('SchedulePanel')
-        await joplin.views.dialogs.addScript(panel, './GUI/Panel/Panel.js')
-        await joplin.views.dialogs.addScript(panel, './GUI/Panel/Panel.css')
-    
+        panel = await joplin.views.panels.create('agendaPanel')
+        //await joplin.views.dialogs.addScript(panel, './GUI/Panel/Panel.js')
+        //await joplin.views.dialogs.addScript(panel, './GUI/Panel/Panel.css')
     }
     async function setupSettings(){
-        await joplin.settings.registerSection("schedule", {
-            name: "Settings",
-            description: "settings for schedule",
-            label: "Settings",
+        await joplin.settings.registerSection("agenda", {
+            name: "agendaSettingsSection",
+            label: "Agenda",
+            description: "Settings for the agenda Plugin",
             iconName: 'fas fa-calendar'
         })
         await joplin.settings.registerSettings({
-            "panel_visibility": {
-                label: "Panel Visibility",
+            "agendaPanelVisibility": {
+                label: "Agenda Panel Visibility",
                 type: SettingItemType.Bool,
                 value: true,
                 public: false,
-                section: "schedule"
+                section: "agenda"
             }}
         )
     }
     async function loadPanelVisibility(){
-        await joplin.views.panels.show(panel, await joplin.settings.value('panel_visibility'))
-    
+        await joplin.views.panels.show(panel, await joplin.settings.value('agendaPanelVisibility'))
     }
     async function registerCommands(){
         await joplin.commands.register({                        // Register the open panel dialog command
-            name: 'toggleSchedulePanel',                          // Set the name of the command
-            label: 'Toggle Schedule Panel',                       // Set the label for the command
+            name: 'toggleAgendaPanel',                          // Set the name of the command
+            label: 'Toggle Agenda Panel',                       // Set the label for the command
             iconName: 'fas fa-calendar',                        // Set the icon for the command
             execute: togglePanelVisibility,
         });
     }
     async function createToolbarButton(){
         await joplin.views.toolbarButtons.create(                   // Creat the open dialog button and bind it to the command
-            'toggleSchedulePanelButton',                           // The name of the button
-            'toggleSchedulePanel',                                 // The above command to bind the button to
+            'toggleAgendaPanelButton',                           // The name of the button
+            'toggleAgendaPanel',                                 // The above command to bind the button to
             ToolbarButtonLocation.NoteToolbar                       // The location of the new button (on the note toolbar)
         );
     }
@@ -65,16 +64,40 @@ Shows the panel if it is visible and vice versa
 async function togglePanelVisibility(){
     var visibility = await joplin.views.panels.visible(panel)
     await joplin.views.panels.show(panel, !visibility)
-    await joplin.settings.setValue('panel_visibility', !visibility)
+    await joplin.settings.setValue('agendaPanelVisibility', !visibility)
+    await updatePanelData();
 }
 
 export async function updatePanelData(){
-    const PanelHTML = await require('./Panel.html').default;
+    var formatted_todos = "<h1>Schedule</h1>"
+    for (var todoDates of (await getTodos()).entries()){
+        console.log(todoDates)
+        var dateHeading = `<h2>${todoDates[0]}</h2>`
+        formatted_todos = formatted_todos.concat(dateHeading)    
+        for (var todo of todoDates[1]){
+            var formatted_html = await getTodoHTML(todo)
+            formatted_todos = formatted_todos.concat(formatted_html)    
+        }
+    }
+    //const PanelHTML = await require('./Panel.html').default;
    //var replacedHTML = DialogHTML.replace("RECURRENCE_DATA", btoa(recurrenceData.toJSON()))
-    await joplin.views.dialogs.setHtml(panel, PanelHTML);
+    await joplin.views.panels.setHtml(panel, formatted_todos);
 }
 
 
+export async function getTodoHTML(todo){
+    var date = new Date(todo.todo_due);
+    var timeString = `${date.getHours().toString().padStart(2, "0")}:${date.getMinutes().toString().padStart(2, "0")}`
+    var htmlTemplate = `
+        <p>
+            <input type="hidden" id="inputTaskID" value="${todo.id}">
+            <input id="completedCheckbox" type="checkbox">
+            <label id="dueDate">${timeString}</label> - 
+            <a id="taskTitle" onclick="onTaskClicked(${todo.id})">${todo.title}</a>
+        </p>
+    `
+    return htmlTemplate
+}
 
 
 
