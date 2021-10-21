838acc3df404cde65f055fe4d94cb6156a398823
BeatLink <beatlink+git@simplelogin.co>
2021-10-21 18:01:10 -0500

    Remove redundant code

--- src/GUI/Panel/Panel.js
+++ src/GUI/Panel/Panel.js
@@ -1,64 +1,13 @@
-
-/******************************************************************************************************************************************
-************************************************************* Stop Data *******************************************************************
-******************************************************************************************************************************************/
-
-
+/* onTodoClicked ************************************************************************************************************************************
+    When a todo item is clicked, this function sends a message to the main plugin containing the todo id
+*/ 
 async function onTodoClicked(todoID){
     await webviewApi.postMessage(['todoClicked', todoID]);
 }
 
+/* onTodoChecked ************************************************************************************************************************************
+    When a todo item is checked as complete/incomplete, this function sends a message to the main plugin containing the todo id
+*/ 
 async function onTodoChecked(todoID){
     await webviewApi.postMessage(['todoChecked', todoID]);
 }
-
-
-
-`
-let weekFieldset = document.getElementById('weekFieldset');
-let weekSundayCheckbox = document.getElementById('weekSundayCheckbox')
-let weekMondayCheckbox = document.getElementById('weekMondayCheckbox')
-let weekTuesdayCheckbox = document.getElementById('weekTuesdayCheckbox')
-let weekWednesdayCheckbox = document.getElementById('weekWednesdayCheckbox')
-let weekThursdayCheckbox = document.getElementById('weekThursdayCheckbox')
-let weekFridayCheckbox = document.getElementById('weekFridayCheckbox')
-let weekSaturdayCheckbox = document.getElementById('weekSaturdayCheckbox')
-weekSundayCheckbox.addEventListener("change", onWeekSundayCheckboxChanged);
-weekMondayCheckbox.addEventListener("change", onWeekMondayCheckboxChanged);
-weekTuesdayCheckbox.addEventListener("change", onWeekTuesdayCheckboxChanged);
-weekWednesdayCheckbox.addEventListener("change", onWeekWednesdayCheckboxChanged);
-weekThursdayCheckbox.addEventListener("change", onWeekThursdayCheckboxChanged);
-weekFridayCheckbox.addEventListener("change", onWeekFridayCheckboxChanged);
-weekSaturdayCheckbox.addEventListener("change", onWeekSaturdayCheckboxChanged);
-
-function onWeekSundayCheckboxChanged(){
-    recurrence.weekSunday = weekSundayCheckbox.checked
-    saveData()
-}
-function onWeekMondayCheckboxChanged(){
-    recurrence.weekMonday = weekMondayCheckbox.checked
-    saveData()
-}
-function onWeekTuesdayCheckboxChanged(){
-    recurrence.weekTuesday = weekTuesdayCheckbox.checked
-    saveData()
-}
-function onWeekWednesdayCheckboxChanged(){
-    recurrence.weekWednesday = weekWednesdayCheckbox.checked
-    saveData()
-}
-function onWeekThursdayCheckboxChanged(){
-    recurrence.weekThursday = weekThursdayCheckbox.checked
-    saveData()
-}
-function onWeekFridayCheckboxChanged(){
-    recurrence.weekFriday = weekFridayCheckbox.checked
-    saveData()
-}
-function onWeekSaturdayCheckboxChanged(){
-    recurrence.weekSaturday = weekSaturdayCheckbox.checked
-    saveData()
-}
-
-
-`
