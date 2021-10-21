3c2edc05824ff7a24600ce6b12422ff5bf5dadbb
BeatLink <beatlink+git@simplelogin.co>
2021-10-20 21:02:08 -0500

    Remove redundant code from core

--- src/Logic/core.ts
+++ src/Logic/core.ts
@@ -1,15 +1,8 @@
 /* Imports *******************************************************************************************************************************/
-import joplin from 'api';
 import { setupPanel } from '../GUI/Panel/Panel';
-//import { setupDatabase, getRecord, updateRecord } from './database';
-import { getSelectedNote } from './joplin';
-import { reviewCompletedTasks } from './completion'
 
 
 /* Main **********************************************************************************************************************************/
 export async function main() {
-    await console.info('Repeating To-Dos Plugin started!');             // Log startup to console
-    //await setupDatabase();                                              // Setup Database
-    await setupPanel();                                          // Setup Dialog Button
-    await reviewCompletedTasks()                                         // Setup task completion logic
+    await setupPanel();                                          // Setup Panel
 }
